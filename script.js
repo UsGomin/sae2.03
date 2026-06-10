c371b7e04994a54caeca8734407ead19944dd25f
UsGomin <antoine.schoubert2@gmail.com>
2026-06-10 16:51:59 +0200

    changement balise dialog en div pour ancienne version firefox

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const BoutonConnexion = document.getElementById('button-connexion');
-const BoutonCreer = document.getElementById('button-creer')
+const BoutonCreer = document.getElementById('button-creer');
 const LoginModal = document.getElementById('login-modal');
 const LoginContent = document.getElementById('login-content');
 const FenConnexion = document.querySelector('.connexion');
 const FenCreer = document.querySelector('.creercompte');
 const BoutonClose = document.getElementById('button-close');
 
-
 BoutonConnexion.addEventListener("click", ()=>{
     const html = FenConnexion.innerHTML;
     LoginContent.innerHTML = html;
 
-    LoginModal.showModal();
+    LoginModal.style.display="flex";
 });
 
 
@@ -19,12 +18,12 @@ BoutonCreer.addEventListener("click", ()=>{
     const html = FenCreer.innerHTML;
     LoginContent.innerHTML = html;
 
-    LoginModal.showModal();
+    LoginModal.style.display="flex";
 });
 
 LoginModal.addEventListener("click", (e)=>{
     if(e.target === BoutonClose){
-        LoginModal.close();
+        LoginModal.style.display="none";
     }
 });
 
