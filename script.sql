31daf9e2d5610fd1bb4a8d4dc9d044061cb39af9
Timoté Le Plapous <timote.leplapous@etudiant.univ-rennes1.fr>
2026-06-16 11:07:55 +0200

    +login +token

--- script.sql
+++ script.sql
@@ -1,9 +1,12 @@
 CREATE TABLE utilisateur(
     id_utilisateur SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    nom VARCHAR(255) NOT NULL,
-    prenom VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    login    VARCHAR(255) NOT NULL,
+    email    VARCHAR(255) NOT NULL,
+    nom      VARCHAR(255) NOT NULL,
+    prenom   VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,   
+    valide   TINYINT NOT NULL DEFAULT 0,
+    token    VARCHAR(64) DEFAULT NULL,
+    UNIQUE (email),
+    UNIQUE (login)
 );
-
-ALTER TABLE utilisateur ADD CONSTRAINT unique_email UNIQUE (email);
